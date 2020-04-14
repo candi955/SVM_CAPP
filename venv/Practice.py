d46c249d3daae5f18476603a434712358e9fcf7a
candi955 <44098788+candi955@users.noreply.github.com>
2020-04-14 15:26:39 -0700

    Updated practice file

--- venv/Practice.py
+++ venv/Practice.py
@@ -191,4 +191,24 @@ print(updatedhelloascii)
 # b'hello\x00\x00\x00\x00\x00\x00\x00'
 # b'hello\x00\x00\x00\x00\x00\x00\x00'
 
+print('\n\n')
+ChinaBytes = int.from_bytes(b'China', byteorder=sys.byteorder)  # creating an int from bytes
+print(ChinaBytes)
+ChinaAscii = int.to_bytes(ChinaBytes, length=20, byteorder=sys.byteorder)
+print(ChinaAscii)
+
+# output is:
+# 418464229443
+# b'China\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+print('\n\n')
+USABytes = int.from_bytes(b'USA', byteorder=sys.byteorder)  # creating an int from bytes
+print(USABytes)
+USAAscii = int.to_bytes(USABytes, length=20, byteorder=sys.byteorder)
+print(USAAscii)
+
+# output is:
+# 4281173
+# b'USA\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
 
