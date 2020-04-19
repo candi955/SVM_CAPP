21cc0bbdc1c59f25f1f286c20653138ee5a57736
candi955 <44098788+candi955@users.noreply.github.com>
2020-04-19 01:33:32 -0700

    Updated dictionary templates on the dictionary list 1 and 2 pages to set up for lists one and two on the main program's third tab (origin and targets).

---
+++ venv/List2DictionaryPage.py
@@ -0,0 +1,105 @@
+# List 2 Dictionary page
+
+
+import pandas as pd
+
+
+#------------ListBox Country/Region/binary Dictionary--------------------------------------------------------------------------
+# creating a dictionary to associate locations with associated binary
+# reference: https://www.w3schools.com/python/python_dictionaries.asp
+
+listTwoDictionary = {"China":{"binary": 418464229443},
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629},
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}
+                "USA":{"binary" : 4281173},
+                "Russia":{"binary" : 107105536406866},
+                "Asia":{"binary" : 1634300737},
+                "Europe":{"binary" : 111533580514629}}
+
+
+pdDict = pd.DataFrame(locationDict)
+pd.set_option('display.max_rows', 1000) # Attempting to display all rows and columns
+pd.set_option('display.max_columns', 1000)
+pd.set_option('display.width', 1000)
